097a524f2c2f45a12a105944ed5058c5095f8d17
ederecl13 <ederecl13@gmail.com>
2018-10-04 16:07:26 -0500

    added code for section 3 of the course

---
+++ basicsyntax/string_methods2.py
@@ -0,0 +1,11 @@
+#Replace Method
+a = "1abc2abc3abc4abc"
+print(a.replace('abc', 'ABC', 2))
+
+#Sub-Strings
+#starting index is inclusve
+sub = a[1:6]
+step = a[1:6:2]
+
+print(sub)
+print(step)
---
+++ basicsyntax/strings.py
@@ -0,0 +1,15 @@
+a = "This is a simple string"
+b = 'Using single quotes'
+
+print(a)
+print(b)
+
+c = "Need to use 'quotes' inside a string"
+print(c)
+
+d = "Another way to handle \"quotes\""
+print(d)
+
+a = "This a single \
+string"
+print(a)
